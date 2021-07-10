53a943360a5cf4c16787dad4fcda47394564f287
Max Tyrrell <maxtyrrell0@gmail.com>
2021-07-10 15:36:25 +0200

    feat(frontend): track actual `unlockStatus` from the blockchain

--- frontend-react/src/components/CreateRaffleForm/CreateRaffleForm.jsx
+++ frontend-react/src/components/CreateRaffleForm/CreateRaffleForm.jsx
@@ -10,33 +10,19 @@ import Nft from "/artifacts/contracts/Nft.sol/Nft.json";
 
 // NFT_CONTRACT_ADDRESS = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
 
-function useNftContract(nftContractAddress) {
-  const { signer } = useContracts();
-
-  const [nftContract, setNftContract] = useState();
-
-  useEffect(() => {
-    if (!signer) {
-      console.error("oh shit, can't setup a contract with a null signer");
-
-      return nftContract;
-    }
-
-    const contract = new ethers.Contract(nftContractAddress, Nft.abi, signer);
-    setNftContract(contract);
-  }, [signer]);
-
-  return nftContract;
-}
-
 export default function CreateRaffleForm() {
-  const { signer, createRaffle, approveNftTransfer, RAFFLE_CONTRACT_ADDRESS } =
-    useContracts();
+  const {
+    signer,
+    createRaffle,
+    raffleContract,
+    approveNftTransfer,
+    RAFFLE_CONTRACT_ADDRESS,
+  } = useContracts();
 
   const [ticketPrice, setTicketPrice] = useState(0);
   const [numTickets, setNumTickets] = useState(0);
 
-  const [nftId, setNftId] = useState(1);
+  const [nftId, setNftId] = useState(5);
   const [nftContractAddress, setNftContractAddress] = useState(
     "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318"
   );
@@ -45,6 +31,15 @@ export default function CreateRaffleForm() {
 
   const [nftIsUnlocked, setNftIsUnlocked] = useState(false);
 
+  // `unlockStatus` can be one of
+  // - "loading" -- loading the approval status of the NFT (will change to one of the statuses below soon)
+  // - "disallowed" -- the connected signer does not own the NFT (TODO: could later allow `approvers` to raffle)
+  // - "locked" -- our raffle contract has not been granted approval for this NFT
+  // - "unlocked" -- raffle contract has been granted approval
+  // - "unlocking" -- the transaction granting our contract approval for this NFT is
+  // ... pending(will change to one of the above statuses soon)
+  const [unlockStatus, setUnlockStatus] = useState("loading");
+
   // On mount, check if the NFT is approved / unlocked
   useEffect(() => {
     // TODO: Listen for on-chain notifications of the Approval event relating to this NFT
@@ -52,13 +47,23 @@ export default function CreateRaffleForm() {
     async function checkApproval() {
       if (nftContract == null) return;
 
+      setUnlockStatus("loading");
       const approved = await nftContract?.getApproved(nftId);
+      const owner = await nftContract?.ownerOf(nftId);
+      const signerAddress = await signer.getAddress();
+
+      console.log("owner is ", owner);
+      console.log("signerAddress is ", signerAddress);
+
+      if (owner !== signerAddress) {
+        setUnlockStatus("disallowed");
+        return;
+      }
 
       if (approved === RAFFLE_CONTRACT_ADDRESS) {
-        setNftIsUnlocked(true);
-        console.log("NFT IS UNLOCKED BBY (approved is: ", approved);
+        setUnlockStatus("unlocked");
       } else {
-        setNftIsUnlocked(false);
+        setUnlockStatus("locked");
       }
 
       console.log("NFT IS approved to: ", approved);
@@ -82,42 +87,40 @@ export default function CreateRaffleForm() {
 
     // TODO: Stop exporting approveNftTransfer from ContractsProvider and
     // instead make a function for this next to useNftContract()
-    let approved = await approveNftTransfer(nftContractAddress, nftId);
+    // let approved = await approveNftTransfer(nftContractAddress, nftId);
 
-    // TODO: get the transaction result
-    //const result = await nftContract.approve(raffleContract.address,ethers.BigNumber.from(nftId))
+    // get the approval transaction result
+    const result = await nftContract.approve(
+      raffleContract.address,
+      ethers.BigNumber.from(nftId)
+    );
+
     // Then display a loading indicator while the transaction is confirmed on the blockchain
-    //setUnlockLoading(true)
-    // Then wait for the transaction to complete...
-    //await transaction.wait()
-    // ...at which point check if the approved address is the raffle contract address
-    // and if so setNftIsUnlocked(true)
+    setUnlockStatus("unlocking");
 
-    if (approved) {
-      // This is fakeness
-      // setNftIsUnlocked(true);
+    console.log("handleUnlockNft: approve transaction result: ", result);
+
+    // Then wait for the transaction to complete...
+    await result.wait();
+    // ...at which point check if the approved address is the raffle contract address
+    const approved = await nftContract?.getApproved(nftId);
+    if (approved === RAFFLE_CONTRACT_ADDRESS) {
+      console.log("unlock successful!");
+      setUnlockStatus("unlocked");
     } else {
-      if (signer) {
-        alert(
-          `It seems like you don't own the nft with id ${nftId}. Are you mistaken? Or just a fucking liar.`
-        );
-      } else {
-        alert(
-          `It seems like you haven't connected your wallet (or reset the state, because we haven't implemented persistent state management), are you really that fucking incompetent?`
-        );
-      }
+      // TODO: Handle if the unlock wasn't successful (eg user rejected transaction, no gas, etc)
+      alert(`NFT unlock failed!. Result: ${result}; approved: ${approved}`);
     }
   }
 
   return (
     <S.Wrapper>
-      <h2>First, let’s unlock the NFT you want to use for this raffle</h2>
       <RaffleForm
         ticketPrice={ticketPrice}
         setTicketPrice={setTicketPrice}
         numTickets={numTickets}
         setNumTickets={setNumTickets}
-        nftIsUnlocked={nftIsUnlocked}
+        unlockStatus={unlockStatus}
         nftId={nftId}
         setNftId={setNftId}
         handleUnlockNft={handleUnlockNft}
@@ -134,7 +137,7 @@ function RaffleForm({
   setTicketPrice,
   numTickets,
   setNumTickets,
-  nftIsUnlocked,
+  unlockStatus,
   nftId,
   setNftId,
   handleUnlockNft,
@@ -142,62 +145,74 @@ function RaffleForm({
   nftContractAddress,
   setNftContractAddress,
 }) {
-  return nftIsUnlocked ? (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Ticket Price:
-        <input
-          type="number"
-          value={ticketPrice}
-          onChange={(e) => {
-            setTicketPrice(e.target.value);
-          }}
+  return (
+    <>
+      <div style={{ opacity: unlockStatus !== "unlocked" ? "1" : "0.3" }}>
+        <h2>
+          Step 1: First, let's unlock the NFT you want to use for this raffle
+        </h2>
+        <UnlockNftForm
+          handleUnlockNft={handleUnlockNft}
+          nftContractAddress={nftContractAddress}
+          setNftContractAddress={setNftContractAddress}
+          nftId={nftId}
+          setNftId={setNftId}
         />
-      </label>
+        {unlockStatus === "loading" && <h3>? Nft lock status loading...</h3>}
+        {unlockStatus === "locked" && <h3>🔒 Nft locked</h3>}
+        {unlockStatus === "unlocked" && <h3>✅ Nft Unlocked</h3>}
+        {unlockStatus === "unlocking" && (
+          <h3 style={{ transition: "all 200ms", transform: "rotate(30deg)" }}>
+            Unlocking NFT
+          </h3>
+        )}
+        {unlockStatus === "disallowed" && <h3>❌ You do not own this NFT</h3>}
+      </div>
 
-      <label>
-        Ticket Quantity:
-        <input
-          type="number"
-          value={numTickets}
-          onChange={(e) => {
-            setNumTickets(e.target.value);
-          }}
-        />
-      </label>
+      <hr />
 
-      <label>
-        NFT id:
-        <input
-          type="number"
-          value={nftId}
-          onChange={(e) => {
-            setNftId(e.target.value);
-          }}
-        />
-      </label>
-      <Button type="submit" value="Create Raffle">
-        Create Raffle
-      </Button>
-      <h3>Total raffle value: {numTickets * ticketPrice}</h3>
-      <h3>✅ Nft Unlocked</h3>
-    </form>
-  ) : (
-    <div>
-      <UnlockNftForm
-        ticketPrice={ticketPrice}
-        setTicketPrice={setTicketPrice}
-        numTickets={numTickets}
-        setNumTickets={setNumTickets}
-        nftIsUnlocked={nftIsUnlocked}
-        nftId={nftId}
-        setNftId={setNftId}
-        handleUnlockNft={handleUnlockNft}
-        nftContractAddress={nftContractAddress}
-        setNftContractAddress={setNftContractAddress}
-      />
-      <h3>🔒 Nft locked</h3>
-    </div>
+      <div style={{ opacity: unlockStatus === "unlocked" ? "1" : "0.3" }}>
+        <h2>Step 2: Next, choose your raffle's number and price of tickets</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Ticket Price:
+            <input
+              type="number"
+              value={ticketPrice}
+              onChange={(e) => {
+                setTicketPrice(e.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Ticket Quantity:
+            <input
+              type="number"
+              value={numTickets}
+              onChange={(e) => {
+                setNumTickets(e.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            NFT id:
+            <input
+              type="number"
+              value={nftId}
+              onChange={(e) => {
+                setNftId(e.target.value);
+              }}
+            />
+          </label>
+          <Button type="submit" value="Create Raffle">
+            Create Raffle
+          </Button>
+          <h3>Total raffle value: {numTickets * ticketPrice}</h3>
+        </form>
+      </div>
+    </>
   );
 }
 
@@ -247,4 +262,23 @@ function UnlockNftForm({
       )}
     </form>
   );
+}
+
+function useNftContract(nftContractAddress) {
+  const { signer, chainId } = useContracts();
+
+  const [nftContract, setNftContract] = useState();
+
+  useEffect(() => {
+    if (!signer) {
+      console.error("oh shit, can't setup a contract with a null signer");
+
+      return nftContract;
+    }
+
+    const contract = new ethers.Contract(nftContractAddress, Nft.abi, signer);
+    setNftContract(contract);
+  }, [signer, chainId]);
+
+  return nftContract;
 }
